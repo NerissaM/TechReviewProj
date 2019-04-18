2135157caa477ea510b36493a341167d85f6ca75
Nerissa Metully <47128742+NerissaM@users.noreply.github.com>
2019-04-18 12:15:44 -0700

    Add files via upload
    Commit models and superuseradmin

--- TechReviewProj/TechReviewApp/admin.py
+++ TechReviewProj/TechReviewApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import TechType, Product, Reivew
 
 # Register your models here.
+admin.site.register(TechType)
+admin.site.register(Product)
+admin.site.register(Reivew)
