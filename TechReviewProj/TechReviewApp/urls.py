de55e83770c65c8172f208d9bb89bd0cad589c1b
Nerissa Metully <47128742+NerissaM@users.noreply.github.com>
2019-04-30 12:11:55 -0700

    Commit Products and Users

--- TechReviewProj/TechReviewApp/urls.py
+++ TechReviewProj/TechReviewApp/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns=[
     path('', views.index, name='index'),
-    path('getTypes/', views.getTypes, name='types')
+    path('getTypes/', views.getTypes, name='types'),
+    path('getProducts/', views.getProducts, name='products'),
+    path ('productDetail/<int:id>', views.productDetail, name='productdetail'),
 ]
+
